ee9c581a97d4547739bc688fa4d82d5bf0e40fd0
Trent Garner <trentongarner.13@gmail.com>
2025-10-15 17:41:06 -0600

    refactor database

--- db/migrate/20251015044214_create_games.rb
+++ db/migrate/20251015044214_create_games.rb
@@ -1,9 +1,7 @@
 class CreateGames < ActiveRecord::Migration[7.2]
   def change
     create_table :games do |t|
-      t.text :deck_state
-      t.text :player_cards_state
-      t.text :dealer_cards_state
+      t.json :state
       t.string :status
       t.string :outcome_message
 
