d11e91b46f7ab4ce81412e88b5f17cb223d538dc
Trent Garner <trentongarner.13@gmail.com>
2025-10-15 17:44:33 -0600

    file structure created

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,7 @@
 
 ActiveRecord::Schema[7.2].define(version: 2025_10_15_044214) do
   create_table "games", force: :cascade do |t|
-    t.text "deck_state"
-    t.text "player_cards_state"
-    t.text "dealer_cards_state"
+    t.json "state"
     t.string "status"
     t.string "outcome_message"
     t.datetime "created_at", null: false
